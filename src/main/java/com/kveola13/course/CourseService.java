00b77e4a7eb64411781c290a5cd111c8ec459362
ola <kveola13@student.westerdals.no>
2020-08-18 22:09:49 +0200

    course service

---
+++ src/main/java/com/kveola13/course/CourseService.java
@@ -0,0 +1,37 @@
+package com.kveola13.course;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CourseService {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    public List<Course> getCourses(String topicId) {
+        List<Course> courses = new ArrayList<>();
+        courseRepository.findByTopic(topicId)
+                .forEach(courses::add);
+        return courses;
+    }
+
+    public Course getCourse(String id) {
+        return courseRepository.findById(id).get();
+    }
+
+    public void addCourse(Course course) {
+        courseRepository.save(course);
+    }
+
+    public void updateCourse(Course course) {
+        courseRepository.save(course);
+    }
+
+    public void deleteCourse(String id) {
+        courseRepository.deleteById(id);
+    }
+}
