42b7e9f71ff8675aff4be84b3207a8d819990283
ola <kveola13@student.westerdals.no>
2020-08-18 22:09:23 +0200

    course controller

---
+++ src/main/java/com/kveola13/course/CourseController.java
@@ -0,0 +1,41 @@
+package com.kveola13.course;
+
+import com.kveola13.courseapi.topic.Topic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @Autowired
+    private CourseService courseService;
+
+    @RequestMapping("topics/{id}/courses")
+    public List<Course> getAllCourses(@PathVariable String id) {
+        return courseService.getCourses(id);
+    }
+
+    @RequestMapping("topics/{topicId}/courses/{id}")
+    public Course getCourse(@PathVariable String id) {
+        return courseService.getCourse(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/topics/{topicId}/courses")
+    public void addCourse(@RequestBody Course course, @PathVariable String topicId) {
+        course.setTopic(new Topic(topicId, "", ""));
+        courseService.addCourse(course);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{topicId}/courses/{id}")
+    public void updateCourse(@RequestBody Course course, @PathVariable String id, @PathVariable String topicId) {
+        course.setTopic(new Topic(topicId, "", ""));
+        courseService.updateCourse(course);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/courses/{id}")
+    public void deleteCourse(@PathVariable String id) {
+        courseService.deleteCourse(id);
+    }
+}
