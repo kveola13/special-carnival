7843bd864200274d6eb3cc8c378d8e9c76ed9923
ola <kveola13@student.westerdals.no>
2020-08-19 10:48:50 +0200

    added topic repo

---
+++ src/main/java/com/kveola13/courseapi/topic/TopicRepository.java
@@ -0,0 +1,8 @@
+package com.kveola13.courseapi.topic;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String> {
+
+}
